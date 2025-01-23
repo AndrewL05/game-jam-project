b628014c1aa5a986709723a315f1abff523e57ce
birongliu <birongliu2003@gmail.com>
2025-01-23 12:25:16 -0500

    Add null check for dialogue resource in start function

--- addons/dialogue_manager/example_balloon/example_balloon.gd
+++ addons/dialogue_manager/example_balloon/example_balloon.gd
@@ -114,7 +114,7 @@ func _notification(what: int) -> void:
 
 ## Start some dialogue
 func start(dialogue_resource: DialogueResource, title: String, extra_game_states: Array = []) -> void:
-	if not is_node_ready():
+	if not is_node_ready() or not dialogue_resource:
 		await ready
 	temporary_game_states =  [self] + extra_game_states
 	is_waiting_for_input = false
